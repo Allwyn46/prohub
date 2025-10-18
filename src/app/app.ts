eea581467dc33ecdf4c9df8b4c33c2d2551654dc
Allwyn46 <allwyn153@gmail.com>
2025-10-18 19:24:45 +0530

    initial Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,15 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  animations: [
+  trigger('fadeIn', [
+    transition(':enter', [
+      style({ opacity: 0, transform: 'translateY(20px)' }),
+      animate('600ms ease-out', ...) // Change duration
+    ])
+  ])
+],
 })
 export class App {
   protected readonly title = signal('prohub');
